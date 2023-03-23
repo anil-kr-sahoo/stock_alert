5d5fd22f5df080ca836c7ddc771bb78982e3a355
anil-kr-sahoo <anil01081997@gmail.com>
2023-03-24 00:52:14 +0530

    add ubuntu notification sound fix positive or negative day changes convert string values to float add csv writer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from time import sleep
+import contextlib
+import os
 import json
+import csv
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -12,28 +14,46 @@ options.add_argument('--remote-debugging-port=61625')
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
 
-def send_notificaations(title, message):
+
+def send_notifications(title, message):
     notification.notify(
         title=title,
         message=message,
         timeout=2
     )
+
+    # For ubuntu, as it has not any sound on desktop notification
+    # "sudo apt install sox"
+    with contextlib.suppress(Exception):
+        os.system('spd-say "hey, you got notification from stock alert" ')
+
+
 def get_two_decimal_val(decimal_num):
     return float('%.2f' % decimal_num)
+
+
+def get_float_val(string_num):
+    try:
+        return float(string_num)
+    except Exception:
+        return 0
+
 
 def get_current_stock_price():
     raw_html = driver.find_element(By.CLASS_NAME, "lpu38Pri").get_attribute('innerHTML').split('hidden;">')[1:]
     raw_amount = ''.join(all_raw.split('</span>')[0] for all_raw in raw_html)
     try:
-        return get_two_decimal_val(float(raw_amount)/100)
+        return get_two_decimal_val(get_float_val(raw_amount) / 100)
     except Exception:
         return 0
 
+
 def get_to_be_credit_dividend(stock_price, dividend_ratio):
     try:
-        return get_two_decimal_val(float(stock_price)*float(dividend_ratio)/100)
+        return get_two_decimal_val(get_float_val(stock_price) * get_float_val(dividend_ratio) / 100)
     except Exception as e:
         return 0
+
 
 def get_stock_details(all_data):
     divident_ratio_percentage = 0
@@ -42,27 +62,31 @@ def get_stock_details(all_data):
     driver.get(url)
     name = driver.find_element(By.CLASS_NAME, "lpu38Head").text
     current_price = get_current_stock_price()
-    day_returns = driver.find_element(By.CLASS_NAME, "lpu38Day").text.split('(')[1].split(')')[0][:-1]
+    multiplier = 1
+    check_negative_multiplier = driver.find_element(By.CLASS_NAME, "lpu38Day").text[0] == '-'
+    if check_negative_multiplier:
+        multiplier *= -1
+    day_returns = get_float_val(
+        driver.find_element(By.CLASS_NAME, "lpu38Day").text.split('(')[1].split(')')[0][:-1]) * multiplier
     all_details = driver.find_element(By.CLASS_NAME, "ft785TableContainer").text
     stock_details = {"Name": name, "Current Price": current_price, "Day Returns": day_returns}
-    ls = {"Name": name, "Current Price": current_price}
     for each in all_details.split('\n'):
         if 'Dividend' in each or 'ROE' in each:
             if 'Dividend' in each:
-                divident_ratio_percentage = each.split(" ")[-1][:-1]
-            stock_details[(' ').join(each.split(" ")[:-1])] = each.split(" ")[-1][:-1]
-            ls[(' ').join(each.split(" ")[:-1])] = each.split(" ")[-1][:-1]
+                divident_ratio_percentage = get_float_val(each.split(" ")[-1][:-1])
+            stock_details[(' ').join(each.split(" ")[:-1])] = get_float_val(each.split(" ")[-1][:-1])
         elif 'Market' in each:
-            stock_details[(' ').join(each.split(" ")[:-1])] = each.split(" ")[-1][1:-2].replace(',', '')
+            stock_details[(' ').join(each.split(" ")[:-1])] = get_float_val(each.split(" ")[-1][1:-2].replace(',', ''))
         else:
-            stock_details[(' ').join(each.split(" ")[:-1])] = each.split(" ")[-1]
-    ls["Credit Dividend"] = get_to_be_credit_dividend(current_price, divident_ratio_percentage)
-    ls["To Be Credit Dividend"] = get_two_decimal_val(get_to_be_credit_dividend(current_price, divident_ratio_percentage) * stock_qty)
-    ls["Qty"] = stock_qty
+            stock_details[(' ').join(each.split(" ")[:-1])] = get_float_val(each.split(" ")[-1])
+    stock_details["Credit Dividend"] = get_to_be_credit_dividend(current_price, divident_ratio_percentage)
+    stock_details["To Be Credit Dividend"] = get_two_decimal_val(
+        get_to_be_credit_dividend(current_price, divident_ratio_percentage) * stock_qty)
+    stock_details["Qty"] = stock_qty
 
-    print(json.dumps(ls, indent=2))
-    send_notificaations("Stok Details", str(ls))
-    return ls
+    print(json.dumps(stock_details, indent=2))
+    send_notifications("Stok Details", str(stock_details))
+    return stock_details
 
 
 urls = [
@@ -108,28 +132,26 @@ urls = [
     ['https://groww.in/stocks/schneider-electric-infrastructure-ltd', 1],
     ['https://groww.in/stocks/infosys-ltd', 1],
 ]
-nm = [
-    ['https://groww.in/stocks/tata-steel-ltd', 130],
-    ['https://groww.in/stocks/one-communications-ltd', 1],
-    ['https://groww.in/stocks/pb-fintech-ltd', 3],
-    ['https://groww.in/stocks/larsen-toubro-infotech-ltd', 5],
-    ['https://groww.in/stocks/indian-railway-catering-tourism-corpn-ltd', 10],
-    ['https://groww.in/stocks/hdfc-bank-ltd', 3],
-    ['https://groww.in/stocks/happiest-minds-technologies-ltd', 19],
-    ['https://groww.in/stocks/avenue-supermarts-ltd', 1],
-]
 all_data = list()
 for data in urls:
-    if 'idfc' not in data[0]:
-        continue
     driver.execute_script("window.open('about:blank', 'secondtab');")
 
     # It is switching to second tab now
     driver.switch_to.window("secondtab")
     all_data.append(get_stock_details(data))
 driver.quit()
-total = sum(data["To Be Credit Dividend"] for data in all_data)
-print(f"Total devident to be credit is {total}/-")
+
+# Calculate total dividend to get from stocks
+total = get_two_decimal_val(sum(data["To Be Credit Dividend"] for data in all_data))
+print(f"Total evident to be credit is {total}/-")
 
 with open("stock_data.json", "w") as twitter_data_file:
     json.dump(all_data, twitter_data_file, indent=4, sort_keys=True)
+
+
+file = open('stock_details.csv', 'w')
+writer = csv.writer(file)
+heading = [list(all_data[0].keys())]
+stock_details = [list(data.values()) for data in all_data]
+writer.writerows(heading + stock_details)
+file.close()
