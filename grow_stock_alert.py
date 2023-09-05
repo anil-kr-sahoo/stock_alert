e50e9b8b8710e93a4f7af511bf7c93558b41df01
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2023-09-05 16:39:40 +0530

    webdriver version upgrade

--- grow_stock_alert.py
+++ grow_stock_alert.py
@@ -240,11 +240,7 @@ try:
         options.headless = True
         options.add_argument('--remote-debugging-port=61625')
         options.add_argument('--no-sandbox')
-        try:
-            driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
-        except Exception:
-            driver = webdriver.Chrome(executable_path=ChromeDriverManager(version="114.0.5735.90").install(),
-                                      options=options)
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
         for data in urls:
             driver.execute_script("window.open('about:blank', 'secondtab');")
 
