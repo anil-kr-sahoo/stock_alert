0a47b60b4c97d6ff4f6169641e16be0bace3c233
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2023-10-09 15:36:31 +0530

    stock update

--- weekly_update.py
+++ weekly_update.py
@@ -1,22 +1,7 @@
 stocks_dict = {
-    "trigger_date": "06/10/2023",
+    "trigger_date": "13/10/2023",
     "removed_stocks": [
     ],
     "newly_added_stocks": [
-        'Bank of Baroda',
-        'Canara Bank',
-        'Castrol India',
-        'HUDCO',
-        'Manappuram Finance',
-        'Mphasis',
-        'NHPC',
-        'NTPC',
-        'National Aluminium Co',
-        'Nippon Life India AM',
-        'Power Finance Corporation',
-        'REC Limited',
-        'Sun TV Network',
-        'Torrent Power',
-        'Union Bank of India'
     ]
 }
