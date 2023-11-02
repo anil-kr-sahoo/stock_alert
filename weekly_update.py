9722ca184f8ca60b58634694ec94a67ff89d391b
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2023-11-02 15:49:53 +0530

    Stock Update

--- weekly_update.py
+++ weekly_update.py
@@ -1,9 +1,8 @@
 stocks_dict = {
-    "trigger_date": "27/10/2023",
+    "trigger_date": "03/11/2023",
     "removed_stocks": [
     ],
     "newly_added_stocks": [
-        'Godrej Agrovet',
-        'State Bank of India'
+        'Indian Oil Corporation'
     ]
 }
