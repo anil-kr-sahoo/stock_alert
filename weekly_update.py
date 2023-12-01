b1957e11c4eaa81b3ca4c8bc1b0998fcd0f7f362
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2023-12-01 15:48:45 +0530

    Weekly Stock Updates

--- weekly_update.py
+++ weekly_update.py
@@ -2,7 +2,8 @@ stocks_dict = {
     "trigger_date": "01/12/2023",
     "removed_stocks": [
         'Manappuram Finance',
-        'State Bank of India'
+        'State Bank of India',
+        'NHPC',
     ],
     "newly_added_stocks": [
     ]
