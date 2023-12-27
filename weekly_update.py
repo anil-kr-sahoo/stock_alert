3e19a3e07cb07133958c23db251ec3fea5d09425
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2023-12-27 09:33:33 +0530

    Stock Update

--- weekly_update.py
+++ weekly_update.py
@@ -1,6 +1,13 @@
 stocks_dict = {
-    "trigger_date": "22/12/2023",
+    "trigger_date": "29/12/2023",
     "removed_stocks": [
+        "Avenue Supermarts",
+        "Happiest Minds Technologies",
+        "HDFC Bank",
+        "IRCTC",
+        "Paytm",
+        "PolicyBazaar - PB Fintech Ltd.",
+        "Tata Steel"
     ],
     "newly_added_stocks": [
     ]
