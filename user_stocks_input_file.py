8c5a88241191d40652ac370edb229f0ef7d6e5cd
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2023-12-29 15:19:59 +0530

    Stock Update

--- user_stocks_input_file.py
+++ user_stocks_input_file.py
@@ -15,7 +15,7 @@ user_stocks = {
         ['https://groww.in/stocks/canara-bank', 8, 395.01],
         ['https://groww.in/stocks/castrol-india-ltd', 6, 143.16],
         ['https://groww.in/stocks/coal-india-ltd', 20, 268.01],
-        ['https://groww.in/stocks/general-insurance-corporation-of-india-ltd', 18, 234.63],
+        ['https://groww.in/stocks/general-insurance-corporation-of-india-ltd', 20, 241.94],
         ['https://groww.in/stocks/hcl-technologies-ltd', 9, 1093.63],
         ['https://groww.in/stocks/hero-motocorp-ltd', 9, 2815.93],
         ['https://groww.in/stocks/hindustan-zinc-ltd', 5, 324.93],
