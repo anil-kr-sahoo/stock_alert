48f4cb5e56a70f76ce3047984ae0a9503c9fcb22
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-02-13 20:33:10 +0530

    stocks update

--- weekly_update.py
+++ weekly_update.py
@@ -2,6 +2,8 @@ stocks_dict = {
     "trigger_date": "16/02/2024",
     "removed_stocks": [
         'Castrol India',
+        'General Insurance',
+        'Bosch'
     ],
     "newly_added_stocks": [
     ]
