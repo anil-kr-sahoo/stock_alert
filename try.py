1f2368db514b0deb7e506d7f33c95fcb8d2a8362
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-07-11 12:50:22 +0530

    python practice added

---
+++ try.py
@@ -0,0 +1,28 @@
+# name= input("enter name: ")
+# age= int(input("enter age: "))
+# mark= float(input("mark: "))
+# print("welcome", name)
+# print("age", age)
+# print("mark", mark)
+
+
+# first= int(input("enter first= "))
+# second= int(input("enter second= "))
+# print("sum= ", first+second)
+
+# first= float(input("enter first= "))
+# second= float(input("enter second= "))
+# print("sum= ", first+second)
+
+# side= int(input("side of the square= "))
+# print("area of the square= ",side*side)
+# side= float(input("side of the square= "))
+# print("area of the square= ", side**2)
+#
+# a= float(input("enter first= "))
+# b= float(input("enter second= "))
+# print("average= ", (a+b)/2)
+
+a= int(input("enter first= "))
+b= int(input("enter second= "))
+print(a>=b)
