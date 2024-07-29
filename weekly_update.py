e4d1ecbb7e6a774d3497f8125c33253a1fa3e431
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-07-29 10:37:03 +0530

    stock update

--- weekly_update.py
+++ weekly_update.py
@@ -2,6 +2,7 @@ stocks_dict = {
     "trigger_date": "02/08/2024",
     "removed_stocks": [
         "Avenue Supermarts",
+        "Colgate-Palmolive (India)",
         "Happiest Minds Technologies",
         "Paytm"
     ],
