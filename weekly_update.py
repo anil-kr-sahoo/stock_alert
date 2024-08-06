c88dbda43e44fdc322d736f97f16a62b0a8a053d
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-08-06 12:22:47 +0530

    stock updates

--- weekly_update.py
+++ weekly_update.py
@@ -2,6 +2,7 @@ stocks_dict = {
     "trigger_date": "09/08/2024",
     "removed_stocks": [
         "Ajanta Pharma",
+        "GTL Infrastructure",
         "Sun TV Network"
     ],
     "newly_added_stocks": [
