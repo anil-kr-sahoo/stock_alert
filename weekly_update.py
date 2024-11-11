382ecd7b4e841ca361b14c50f68620bbefc158df
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-11-11 08:44:03 +0530

    stock updated

--- weekly_update.py
+++ weekly_update.py
@@ -1,8 +1,9 @@
 stocks_dict = {
-    "trigger_date": "08/11/2024",
+    "trigger_date": "15/11/2024",
     "removed_stocks": [
         "Oil India"
     ],
     "newly_added_stocks": [
+        "Colgate-Palmolive (India)"
     ]
 }
