192eee58dfe40fff257fa5dfe58de7f5fb5c4a3d
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-11-20 17:44:09 +0530

    stock updated

--- user_stocks_input_file.py
+++ user_stocks_input_file.py
@@ -150,11 +150,6 @@ user_stocks = {
             11168.31
         ],
         [
-            "https://groww.in/stocks/oil-india-ltd",
-            0,
-            0
-        ],
-        [
             "https://groww.in/stocks/oil-natural-gas-corporation-ltd",
             48,
             262.55
--- weekly_update.py
+++ weekly_update.py
@@ -1,9 +1,8 @@
 stocks_dict = {
-    "trigger_date": "15/11/2024",
+    "trigger_date": "22/11/2024",
     "removed_stocks": [
+        "Oil India"
     ],
     "newly_added_stocks": [
-        "Colgate-Palmolive (India)",
-        "NTPC"
     ]
 }
