f22d2331d65630c79187d7afe0a8822ec693873e
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2025-01-14 17:29:58 +0530

    stock updated

--- user_stocks_input_file.py
+++ user_stocks_input_file.py
@@ -80,6 +80,11 @@ user_stocks = {
             373.95
         ],
         [
+            "https://groww.in/stocks/housing-urban-development-corporation-ltd",
+            0,
+            0
+        ],
+        [
             "https://groww.in/stocks/idbi-bank-ltd",
             3,
             70.79
