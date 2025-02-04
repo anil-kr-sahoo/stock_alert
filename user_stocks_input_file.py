b19f7d1fff052951666b114ca099dfa93ff434ed
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2025-02-05 00:24:37 +0530

    stock updated

--- user_stocks_input_file.py
+++ user_stocks_input_file.py
@@ -80,6 +80,11 @@ user_stocks = {
             373.95
         ],
         [
+            "https://groww.in/stocks/housing-urban-development-corporation-ltd",
+            0,
+            0
+        ],
+        [
             "https://groww.in/stocks/indraprastha-gas-ltd",
             6,
             208.76
