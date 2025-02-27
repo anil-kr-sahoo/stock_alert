f40b45502c06890802b90729fd4a21356f2c7968
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2025-02-27 16:07:52 +0530

    Stock Update

--- weekly_update.py
+++ weekly_update.py
@@ -1,6 +1,9 @@
 stocks_dict = {
     "trigger_date": "28/02/2025",
     "removed_stocks": [
+        "HUDCO",
+        "IDBI Bank",
+        "Mphasis"
     ],
     "newly_added_stocks": [
         "TCS",
