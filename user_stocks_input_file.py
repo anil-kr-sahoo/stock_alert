98221956b051424b612a27ad97d5c3458f8f1817
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2025-03-25 15:35:17 +0530

    Stock Update

--- user_stocks_input_file.py
+++ user_stocks_input_file.py
@@ -36,8 +36,8 @@ user_stocks = {
         ],
         [
             "https://groww.in/stocks/castrol-india-ltd",
-            52,
-            231.17
+            57,
+            229.15
         ],
         [
             "https://groww.in/stocks/coal-india-ltd",
