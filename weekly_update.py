d3b53f468c33f906bf86f175233d0d0e90f69611
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2025-06-20 16:19:31 +0530

    Stock Update

--- weekly_update.py
+++ weekly_update.py
@@ -1,9 +1,8 @@
 stocks_dict = {
-    "trigger_date": "20/06/2025",
+    "trigger_date": "27/06/2025",
     "removed_stocks": [
-        "State Bank of India"
     ],
     "newly_added_stocks": [
-        "HDFC Asset Management Company"
+        "Page Industries"
     ]
 }
