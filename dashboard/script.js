010573a0593e276b61898088b5e78479671c52c9
anil-kr-sahoo <anil01081997@gmail.com>
2025-06-20 15:13:06 +0530

    stock update

--- dashboard/script.js
+++ dashboard/script.js
@@ -6,13 +6,13 @@ fetch("../stock_data.json")
     let totalReturns = 0;
     let totalQty = 0;
 
-    data.forEach(stock => {
+    stock_data.forEach(stock => {
       totalReturns += parseFloat(stock["Total Returns"]);
       totalQty += parseInt(stock["Qty"]);
     });
     document.getElementById("timestamp").textContent = "Last Updated: " + data.Time;
     document.getElementById("message").textContent = "Total Qty: " + totalQty;
-    document.getElementById("status").textContent = "Total PL: " + totalReturns;
+    document.getElementById("status").textContent = "Total PL: " + totalReturns.toFixed(2) +"/-";
   })
   .catch(() => {
     document.getElementById("message").textContent = "Error loading status.";
