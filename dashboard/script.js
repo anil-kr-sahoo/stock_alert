18eed37594e7cc70e60dece98a6a5779d9f526c8
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2025-06-20 15:18:43 +0530

    Script update

--- dashboard/script.js
+++ dashboard/script.js
@@ -10,10 +10,25 @@ fetch("../stock_data.json")
       totalReturns += parseFloat(stock["Total Returns"]);
       totalQty += parseInt(stock["Qty"]);
     });
-    document.getElementById("timestamp").textContent = "Last Updated: " + data.Time;
+    document.getElementById("timestamp").textContent = "Last Updated: " + formatReadableDateTime(data.Time);
     document.getElementById("message").textContent = "Total Qty: " + totalQty;
     document.getElementById("status").textContent = "Total PL: " + totalReturns.toFixed(2) +"/-";
   })
   .catch(() => {
     document.getElementById("message").textContent = "Error loading status.";
   });
+
+function formatReadableDateTime(dateString) {
+  const date = new Date(dateString.replace(" ", "T")); // Ensure proper parsing
+
+  const options = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  };
+
+  return date.toLocaleString("en-GB", options).replace(",", "");
+}
