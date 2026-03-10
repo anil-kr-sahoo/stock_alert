c7206787f0884e0cef77b35350775adf4c1268b6
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2026-03-10 09:48:27 +0530

    Stock Update

--- weekly_update.py
+++ weekly_update.py
@@ -1,9 +1,7 @@
 stocks_dict = {
-    "trigger_date": "27/02/2026", # DD/MM/YYYY
+    "trigger_date": "13/03/2026", # DD/MM/YYYY
     "removed_stocks": [
         'Indraprastha Gas',
-        'NTPC',
-        'Indian Oil Corporation'
     ],
     "newly_added_stocks": [
     ]
