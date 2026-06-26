8227097aeeead2b505c8a7dea251d3a9e77b17d1
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2026-06-26 11:13:08 +0530

    stock update

--- user_stocks_input_file.py
+++ user_stocks_input_file.py
@@ -228,11 +228,6 @@ user_stocks = {
             212.85
         ],
         [
-            "https://groww.in/stocks/reliance-nippon-life-asset-management-ltd",
-            0,
-            0
-        ],
-        [
             "https://groww.in/stocks/tata-consultancy-services-ltd",
             20,
             2490.20
